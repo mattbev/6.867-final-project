64cd5ee02cadc304e8ae7343557816fcc892e5f2
Matt Beveridge <mattbev@mit.edu>
2020-11-18 21:27:56 -0500

    very simple template from: https://github.com/nbertagnolli/pytorch-simple-gan/

---
+++ models.py
@@ -0,0 +1,21 @@
+from torch import nn
+
+class Generator(nn.Module):
+    def __init__(self, input_length: int):
+        super(Generator, self).__init__()
+        self.dense = nn.Linear(int(input_length), int(input_length))
+        self.activation = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.activation(self.dense(x))
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self, input_length: int):
+        super(Discriminator, self).__init__()
+        self.dense = nn.Linear(int(input_length), 1)
+        self.activation = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.activation(self.dense(x))
