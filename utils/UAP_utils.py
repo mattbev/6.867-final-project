4a61dedd49a6c6f72bec9362033bef05e3ac8d65
vmorestes <36943933+vmorestes@users.noreply.github.com>
2020-12-09 11:43:20 -0300

    Create UAP_utils.py

---
+++ utils/UAP_utils.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: victor
+"""
+import torch
+import torch.nn as nn
+
+
+class UAP(nn.Module):
+    def __init__(self,
+                shape=(28, 28),
+                num_channels=1,
+                mean=[0.5],
+                std=[0.5],
+                use_cuda=True):
+        super(UAP, self).__init__()
+
+        self.use_cuda = use_cuda
+        self.num_channels = num_channels
+        self.shape = shape
+        self.uap = nn.Parameter(torch.zeros(size=(num_channels, *shape), requires_grad=True))
+
+        self.mean_tensor = torch.ones(1, num_channels, *shape)
+        for idx in range(num_channels):
+            self.mean_tensor[:,idx] *= mean[idx]
+        if use_cuda:
+            self.mean_tensor = self.mean_tensor.cuda()
+
+        self.std_tensor = torch.ones(1, num_channels, *shape)
+        for idx in range(num_channels):
+            self.std_tensor[:,idx] *= std[idx]
+        if use_cuda:
+            self.std_tensor = self.std_tensor.cuda()
